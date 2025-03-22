f183f25ad4fa4ce00bc8598072805e4dbddfbbdf
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-22 04:06:00 -0300

    use pycord in dev shell

--- shell.nix
+++ shell.nix
@@ -3,12 +3,28 @@
     config = { };
     overlays = [ ];
   },
+  ...
 }:
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    python312
-    python312Packages.pip
-    python312Packages.discordpy
+let
+  pythonPackages = pkgs.python313Packages;
+  pycord =
+    let
+      pname = "py_cord";
+      version = "2.6.1";
+    in
+    pythonPackages.buildPythonPackage {
+      doCheck = false;
+      inherit pname version;
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-NgZPIl8se73f5ULV7VgfKldE9hjgOQk8980mWaWLx5s=";
+      };
+    };
+in
+pkgs.mkShell {
+  packages = with pythonPackages; [
+    python
+    pip
+    pycord
   ];
-  # shellHook = "python main.py";
 }
