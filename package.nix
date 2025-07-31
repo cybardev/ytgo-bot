3bafa9c30e4b309f78933882fdc3da34a1edd97f
cybardev <50134239+cybardev@users.noreply.github.com>
2025-07-30 21:31:29 -0300

    use latest pycord

--- package.nix
+++ package.nix
@@ -1,32 +1,42 @@
 { pkgs, ... }:
 let
-  python = pkgs.python312;
-  pythonPackages = pkgs.python312Packages;
+  python = pkgs.python313;
+  pythonPackages = pkgs.python313Packages;
   pycord =
     let
       pname = "py_cord";
-      version = "2.6.1";
+      version = "2.6.2";
     in
     pythonPackages.buildPythonPackage {
+      pyproject = true;
+      build-system = with pythonPackages; [
+        setuptools
+        setuptools-scm
+      ];
       doCheck = false;
       inherit pname version;
       src = pkgs.fetchFromGitHub {
         owner = "Pycord-Development";
         repo = "pycord";
-        tag = "v${version}";
-        hash = "sha256-35GfgXmTED3/jmshRwtRXPns+UkeKX2tG1ksGTfwjnY=";
+        rev = "bc287f4f9286646151017296b9ba431728381b18";
+        hash = "sha256-084VAx1b+0U7zSKDnS2kf8M6BAL9yGjpTGhk/q3QGcg=";
       };
+      propagatedBuildInputs = with pythonPackages; [
+        aiohttp
+        faust-cchardet
+        msgspec
+        typing-extensions
+        yarl
+      ];
     };
 in
 python.pkgs.buildPythonApplication {
   pname = "ytgo-bot";
   version = "1.2.1";
-  propagatedBuildInputs = with pythonPackages; [
+  pyproject = true;
+  build-system = [ pythonPackages.setuptools ];
+  propagatedBuildInputs = [
     pycord
-    aiohttp
-    faust-cchardet
-    msgspec
-    yarl
     (pkgs.cy.ytgo.overrideAttrs (
       finalAttrs: previousAttrs: {
         postFixup = "";
